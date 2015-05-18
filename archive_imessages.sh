0a59ff0ad278c846c46031e1d81c301baec0ef44
5c0tt <scott@newgeo.com>
2015-05-17 19:34:47 -0700

    Changed file path to correct folder location

--- archive_imessages.sh
+++ archive_imessages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Finds all attachments in ~/Library/Messages from iMessage and saves an archive of them
-#Archive is stored at /Users/$USER/Library/Messages/Extracted-Files/archived_files
+#Archive is stored at /Users/$USER/Library/Messages/Scripts\ and\ Archives/archived_files
 
 
 echo -e "\n -- Archive iMessage attachment images --\n"
@@ -14,15 +14,15 @@ echo -e "2) Now in $(pwd)"
 # Make a timestamp file that is a timestamp so I can know the last time this ran, touch it to update the timestamp
 echo -e "3) Finding all files newer than your timestamp file, copying them to a final storage location."
 
-if [ ! -f "/Users/$USER/Library/Messages/Extracted-Files/timestamp" ]; then
+if [ ! -f "/Users/$USER/Library/Messages/Scripts\ and\ Archives/timestamp" ]; then
 	echo "     -Timestamp file DOES NOT EXIST"
-	find /Users/$USER/Library/Messages/Attachments -not -name ".DS_Store" -not -name "timestamp" -name "*" -type f -exec cp {} /Users/$USER/Library/Messages/Extracted-Files/archived_files \;
+	find /Users/$USER/Library/Messages/Attachments -not -name ".DS_Store" -name "*" -type f -exec cp {} /Users/$USER/Library/Messages/Scripts\ and\ Archives/archived_files \;
 else
 	echo "     -TIMESTAMP file EXISTS"
-	find /Users/$USER/Library/Messages/Attachments -not -name ".DS_Store" -not -name "timestamp" -name "*" -type f -newer /Users/$USER/Library/Messages/Extracted-Files/timestamp -exec cp {} /Users/$USER/Library/Messages/Extracted-Files/archived_files \;
+	find /Users/$USER/Library/Messages/Attachments -not -name ".DS_Store" -not -name "timestamp" -name "*" -type f -newer /Users/$USER/Library/Messages/Scripts\ and\ Archives/timestamp -exec cp {} /Users/$USER/Library/Messages/Scripts\ and\ Archives/archived_files \;
 fi
 
 # Update the timestamp
 echo -e "4) Update the timestamp for the next run\n"
-touch /Users/$USER/Library/Messages/Extracted-Files/timestamp
+touch /Users/$USER/Library/Messages/Scripts\ and\ Archives/timestamp
 
